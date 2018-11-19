d3e99964fde112304ebfc3bf105d3d79bc80a6a5
taylor.monacelli <taylor.monacelli@servicenow.com>
2018-11-18 16:07:54 -0800

    metadata

--- timecard.js
+++ timecard.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Wand Pro timecard
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Taylor Monacelli
 // @match        https://prowand.pro-unlimited.com/worker/standard/billing/billingedit/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
