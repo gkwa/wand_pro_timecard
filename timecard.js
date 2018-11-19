1e51daf333c070b882ab55e1913e60a049ad2778
taylor.monacelli <taylor.monacelli@servicenow.com>
2018-11-18 17:29:30 -0800

    Basics are working

--- timecard.js
+++ timecard.js
@@ -10,11 +10,58 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  "use strict";
 
-    // Your code here...
-    var $ = window.jQuery;
-    $(document).ready(function() {
-        alert('WINNING');
-    });
+  var $ = window.jQuery;
+  $(document).ready(function() {
+    function setShiftType(
+      billingDetailItems,
+      billingTimeSpans,
+      timeEntrySpanType
+    ) {
+      var s = `#billingDetailItems${billingDetailItems}\\2e billingTimeSpans${billingTimeSpans}\\2e timeEntrySpanType`;
+      $(s).val(timeEntrySpanType);
+    }
+
+    function addTimeEntry(
+      billingDetailItems,
+      billingTimeSpans,
+      start_or_end,
+      hour,
+      minute,
+      ampm
+    ) {
+      var s;
+
+      s = `#billingDetailItems${billingDetailItems}\\2e billingTimeSpans${billingTimeSpans}\\2e ${start_or_end}HourM`;
+      $(s).val(hour);
+      s = `#billingDetailItems${billingDetailItems}\\2e billingTimeSpans${billingTimeSpans}\\2e ${start_or_end}Minute`;
+      $(s).val(minute);
+      s = `#billingDetailItems${billingDetailItems}\\2e billingTimeSpans${billingTimeSpans}\\2e ${start_or_end}Meridiem`;
+      $(s).val(ampm);
+    }
+
+    var day;
+    var amPmEnum = {
+      AM: 0,
+      PM: 1
+    };
+
+    for (day = 0; day <= 4; day++) {
+      // morning shift
+      addTimeEntry(day, 0, "start", 9, 0, amPmEnum.AM);
+      addTimeEntry(day, 0, "end", 12, 0, amPmEnum.PM);
+      setShiftType(day, 0, "Labor");
+
+      // lunch
+      addTimeEntry(day, 1, "start", 12, 0, amPmEnum.PM);
+      addTimeEntry(day, 1, "end", 1, 0, amPmEnum.PM);
+      setShiftType(day, 1, "Lunch");
+
+      // afternoon shift
+      addTimeEntry(day, 2, "start", 1, 0, amPmEnum.PM);
+      addTimeEntry(day, 2, "end", 6, 0, amPmEnum.PM);
+      setShiftType(day, 2, "Labor");
+    }
+  });
 })();
