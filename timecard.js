20325ab069ced75fce70b2b347440e17aa12ec32
taylor.monacelli <taylor.monacelli@servicenow.com>
2018-11-18 16:07:45 -0800

    Limit script to running on just one page

--- timecard.js
+++ timecard.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        https://prowand.pro-unlimited.com/worker/standard/billing/billingedit/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
